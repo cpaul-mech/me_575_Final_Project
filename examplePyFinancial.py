68c6cbfe9d2cf0aad4fd773872c39b29cec1a928
Christopher Paul <124709632+cpaul-mech@users.noreply.github.com>
2025-03-26 21:21:04 -0600

    update requirements.txt

--- examplePyFinancial.py
+++ examplePyFinancial.py
@@ -5,5 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # now we will practice getting stock data!!
-#%%
-tsla = yf.Ticker("TSLA")
+#%% ## Lets start off by downloading the stock data for Tesla (TSLA) for the past 5 years and storing it locally
+# we will use the yfinance library to do this
+tsla = yf.Ticker("TSLA") # Create a Ticker object to access Tesla stock data
+tsla_hist = tsla.history(period="5y") # Get the historical stock data for the past 5 years
+tsla_hist.to_csv("tsla_hist.csv") # Save the data to a csv file
