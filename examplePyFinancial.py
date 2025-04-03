424356a2782f0b66c44304bd1be49667fea646d1
Christopher Paul <124709632+cpaul-mech@users.noreply.github.com>
2025-04-03 15:02:21 -0600

    added file checking functionality and added more stock data to graph

--- examplePyFinancial.py
+++ examplePyFinancial.py
@@ -3,18 +3,49 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+import os
 # now we will practice getting stock data!!
 #%% ## Lets start off by downloading the stock data for Tesla (TSLA) for the past 5 years and storing it locally
 # we will use the yfinance library to do this
-tsla = yf.Ticker("TSLA") # Create a Ticker object to access Tesla stock data
-tsla_hist = tsla.history(period="5y") # Get the historical stock data for the past 5 years
-tsla_hist.to_csv("tsla_hist.csv") # Save the data to a csv file
+#check to see if the historical data has already been downloaded and saved locally
+stock_names = [
+    "TSLA", # Tesla Inc.
+    "AAPL", # Apple Inc.
+    "MSFT", # Microsoft Corporation
+    "AMZN", # Amazon.com Inc.
+    "GOOGL", # Alphabet Inc. (Google)
+    "TERA", # Teradyne Inc.
+    "PWR", # Quanta Services Inc.
+    "NVDA", # NVIDIA Corporation *note: more of a volatile stock apparently.
+    "ZION", # Zions Bancorporation
+    "RIVN", # Rivian Automotive Inc. * Emerging sector company
+    "PLTR", # Palantir Technologies Inc.
+    "MRNA", # Moderna Inc. * note the covid bumb?
+]
+#%% load data from file if it exists, otherwise download it.
+fin_hist_data = {} # Initialize an empty dictionary to store the historical data for each stock
+# key is stock name, value is the historical data (DataFrame)
+for stock in stock_names:
+    if os.path.exists(f"{stock}_hist.csv"):
+        print(f"File {stock}_hist.csv already exists. Loading data...")
+        fin_hist = pd.read_csv(f"{stock}_hist.csv", index_col=0, parse_dates=True)
+        fin_hist_data[stock] = fin_hist
+    else:
+        print(f"{stock} data not found. Downloading historical data...")
+        ticker = yf.Ticker(stock) # Create a Ticker object to access stock data
+        ticker_history = ticker.history(period="5y")
+        ticker_history.to_csv(f"{stock}_hist.csv") # Save the data to a csv file
+        fin_hist_data[stock] = ticker_history
+        print(f"Historical data for {stock} downloaded and saved to file.")
 
 #%% ## Now let's go ahead and graph the data over the last 5 years.
-plt.plot(tsla_hist.index, tsla_hist["Close"]) 
-# The "Close" column contains the closing price of the stock for each day.
-plt.title("Tesla Stock Price Over the Last 5 Years")
+plt.figure(1)
+for stock in stock_names:
+    plt.plot(fin_hist_data[stock].index, fin_hist_data[stock]['Close'], label=stock)
+# The "Close" column contains the closing price of the stock for each day. The closing price is the last price at which the stock traded on that day.
+plt.xticks(rotation=45) # Rotate x-axis labels for better readability
+plt.title("Comparing Stock Prices Over the Last 5 Years")
+plt.legend()
 plt.xlabel("Date")
 plt.ylabel("Stock Price (USD)")
 plt.show() #
